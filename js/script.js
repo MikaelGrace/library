875d95ed6006d07a7ae4cb9e6cbf994d365b7722
MikaelGrace <MikeOkoduwa700@gmail.com>
2025-02-27 09:42:53 +0100

    add button that displays a form for a new book when clicked - add button to show all the books in th library

--- js/script.js
+++ js/script.js
@@ -1,5 +1,49 @@
 const myLibrary = [];
 const container = document.querySelector(".container");
+const newBookButton = document.querySelector("#new-book");
+const showBooksButton = document.querySelector("#show-books");
+const displayBooksDiv = document.querySelector(".display-books");
+const bookForm = document.querySelector("form");
+let titleinput = document.querySelector("#title");
+let authorInput = document.querySelector("#author");
+let pagesInput = document.querySelector("#pages");
+let readStatus = document.querySelectorAll(".read-status-input");
+const submitForm = document.querySelector("#submit-form");
+
+newBookButton.addEventListener("click", () => {
+    displayBooksDiv.innerHTML = "";
+    displayBooksDiv.style.display = "none";
+    
+    bookForm.style.display = "block";
+
+});
+
+submitForm.addEventListener("click", (e) => {
+
+    e.preventDefault();
+    const title = titleinput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    let read;
+    for (let i = 0; i < readStatus.length; i++) {
+        if (readStatus[i].checked) {
+            read = readStatus[i].value;
+        }
+        
+    }
+
+    //console.log(`${title} ${author} ${pages}`)
+    console.log(read);
+
+    addBookToLibrary(title, author, pages, read);
+    bookForm.reset();
+    bookForm.style.display = "none";
+});
+
+showBooksButton.addEventListener("click", () => {
+    displayBooksDiv.style.display = "block";
+    displayBooks();
+})
 
 
 
@@ -25,20 +69,21 @@ function addBookToLibrary(title, author, pages, read) {
 
 };
 
-addBookToLibrary("Things Fall Apart", "Chinua Achebe", 300, "not yet read");
-addBookToLibrary("A prisoner of birth", "Jeffery Archer", 700, "has been read");
-addBookToLibrary("Death and the king's horseman", "Wole Soyinka", 200, "has been read");
-addBookToLibrary("Purple Hibiscus", "Chimamanda Ngozi Adiche", 400, "not yet read");
+// addBookToLibrary("Things Fall Apart", "Chinua Achebe", 300, "not yet read");
+// addBookToLibrary("A prisoner of birth", "Jeffery Archer", 700, "has been read");
+// addBookToLibrary("Death and the king's horseman", "Wole Soyinka", 200, "has been read");
+// addBookToLibrary("Purple Hibiscus", "Chimamanda Ngozi Adiche", 400, "not yet read");
 
 
 function displayBooks () {
     for (let i = 0; i < myLibrary.length; i++) {
         const book = document.createElement("p");
-        const element = myLibrary[i].title;
+        const element = myLibrary[i].title + " by " + myLibrary[i].author + ". It has " + myLibrary[i].pages + " pages and it " + myLibrary[i].read;
         book.textContent = element;
-        container.appendChild(book);
+        //container.appendChild(book);
+        displayBooksDiv.appendChild(book);
         
     }
 }
 
-displayBooks();
+//displayBooks();
