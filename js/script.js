5a1fe597cdb76c5f1749fd29730042e51dc8de6e
MikaelGrace <MikeOkoduwa700@gmail.com>
2025-02-28 13:19:33 +0100

    add styling to improve the ui

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ let titleinput = document.querySelector("#title");
 let authorInput = document.querySelector("#author");
 let pagesInput = document.querySelector("#pages");
 let readStatus = document.querySelectorAll(".read-status-input");
+let bookImageInput = document.querySelector("#book-image");
 const submitForm = document.querySelector("#submit-form");
 
 newBookButton.addEventListener("click", () => {
@@ -15,6 +16,7 @@ newBookButton.addEventListener("click", () => {
     displayBooksDiv.style.display = "none";
     
     bookForm.style.display = "block";
+    //bookForm.style.gridTemplateColumns = "1fr";
 
 });
 
@@ -24,6 +26,7 @@ submitForm.addEventListener("click", (e) => {
     const title = titleinput.value;
     const author = authorInput.value;
     const pages = pagesInput.value;
+    const file = bookImageInput.files[0];
     let read;
     for (let i = 0; i < readStatus.length; i++) {
         if (readStatus[i].checked) {
@@ -33,11 +36,27 @@ submitForm.addEventListener("click", (e) => {
     }
 
     //console.log(`${title} ${author} ${pages}`)
-    console.log(read);
+    //console.log(read);
 
-    addBookToLibrary(title, author, pages, read);
-    bookForm.reset();
-    bookForm.style.display = "none";
+    // addBookToLibrary(title, author, pages, read);
+    // bookForm.reset();
+    // bookForm.style.display = "none";
+
+    if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            addBookToLibrary(title, author, pages, read, reader.result);
+            bookForm.reset();
+            bookForm.style.display = "none";
+        }
+    }
+    else {
+        addBookToLibrary(title, author, pages, read, null);
+        bookForm.reset();
+        bookForm.style.display = "none";
+
+    }
 });
 
 showBooksButton.addEventListener("click", () => {
@@ -49,11 +68,12 @@ showBooksButton.addEventListener("click", () => {
 
 
 
-function Book (title, author, pages, read) {
+function Book (title, author, pages, read, image) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.image = image;
     this.info = function () {
         return `${title} by ${author}, ${pages}, ${read}`;
     };
@@ -63,9 +83,9 @@ function Book (title, author, pages, read) {
 
 
 
-function addBookToLibrary(title, author, pages, read) {
+function addBookToLibrary(title, author, pages, read, image) {
   // take params, create a book then store it in the array
-  let newBook = new Book(title, author, pages, read);
+  let newBook = new Book(title, author, pages, read, image);
   myLibrary.push(newBook);
 
 };
@@ -83,6 +103,7 @@ function displayBooks () {
         const bookAuthor = document.createElement("p");
         const bookPages = document.createElement("p");
         const bookStatus = document.createElement("p");
+        const bookImage = document.createElement("img");
         const deleteBook = document.createElement("button");
 
         deleteBook.textContent = "Delete Book";
@@ -91,11 +112,15 @@ function displayBooks () {
             
             if (bookTitle.textContent === myLibrary[i].title) {
                 myLibrary.splice(i, 1);
+
+                //displayBooks(); //this can be used instead of removing the bookCard from the displayBooksDiv
                 
             }
             displayBooksDiv.removeChild(bookCard);
         })
         bookTitle.setAttribute("class", "title");
+        bookImage.setAttribute("class", "book-thumbnail");
+        deleteBook.setAttribute("class", "delete-btn");
         bookCard.setAttribute("class", "card");
 
         bookTitle.textContent = myLibrary[i].title;
@@ -103,10 +128,18 @@ function displayBooks () {
         bookPages.textContent = "No. of Pages: " + myLibrary[i].pages + " pages";
         bookStatus.textContent = "Status: " + myLibrary[i].read;
 
+
+        if (myLibrary[i].image) {
+            bookImage.src = myLibrary[i].image;
+            bookImage.alt = "Book Cover";
+            //bookImage.classList("book-thumbnail");
+            bookCard.appendChild(bookImage);
+        }
+
         // const element = myLibrary[i].title + " by " + myLibrary[i].author + ". It has " + myLibrary[i].pages + " pages and it " + myLibrary[i].read;
         // book.textContent = element;
         //container.appendChild(book);
-
+        
         bookCard.appendChild(bookTitle);
         bookCard.appendChild(bookAuthor);
         bookCard.appendChild(bookPages);
