8b964acaa368e12d9f138c6249c2417b25bf4cc5
MikaelGrace <mikeokoduwa700@gmail.com>
2025-03-21 13:34:55 +0100

    refractor the book object to use class instead of plain constructor

--- js/script.js
+++ js/script.js
@@ -68,16 +68,30 @@ showBooksButton.addEventListener("click", () => {
 
 
 
-function Book (title, author, pages, read, image) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.image = image;
-    this.info = function () {
-        return `${title} by ${author}, ${pages}, ${read}`;
-    };
+class Book {
+    constructor(title, author, pages, read, image) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.image = image;
+        this.info = function () {
+            return `${title} by ${author}, ${pages}, ${read}`;
+        };
+    }
 }
+
+
+// function Book (title, author, pages, read, image) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.image = image;
+//     this.info = function () {
+//         return `${title} by ${author}, ${pages}, ${read}`;
+//     };
+// }
 
 
 
